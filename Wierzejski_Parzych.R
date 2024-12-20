cc8cac985b0ff3661340d1a525c1715f9c0a9c66
Hubert Parzych <hubert@parzy.ch>
2024-12-20 02:26:52 +0100

    wip

--- Wierzejski_Parzych.R
+++ Wierzejski_Parzych.R
@@ -126,7 +126,7 @@ ggsave(
 #-----------------------------------------
 
 log_zwroty_nwg <- diff(log(kurs_zamkniecia_nwg))
-log_zwroty_dyn <- diff(log(kurs_zamkniecia_dyn))
+log_zwroty_dt <- diff(log(kurs_zamkniecia_dyn))
 
 # Przycięcie pierwszego elementu kurs_data_nwg
 kurs_data_cut_nwg <- kurs_data_nwg[-1]
@@ -136,7 +136,7 @@ log_zwroty_nwg_df <- data.frame(data = kurs_data_cut_nwg, log_zwroty = log_zwrot
 # Przycięcie pierwszego elementu kurs_data_dyn
 kurs_data_cut_dt <- kurs_data_dyn[-1]
 
-log_zwroty_dt_df <- data.frame(data = kurs_data_cut_dyn, log_zwroty = log_zwroty_dyn)
+log_zwroty_dt_df <- data.frame(data = kurs_data_cut_dyn, log_zwroty = log_zwroty_dt)
 
 histogram_zwrotów_nwg_ggplot <- ggplot(
   log_zwroty_nwg_df,
@@ -201,7 +201,7 @@ ggsave(
 )
 
 #-----------------------------------------
-# Estymacja kwantyli 0.05, 0.5, 0.95
+# [NWG] Estymacja kwantyli 0.05, 0.5, 0.95
 #-----------------------------------------
 # Wartość oczekiwana
 mu_nwg <- mean(log_zwroty_nwg)
@@ -248,10 +248,57 @@ ggsave(
   dpi = 480
 )
 
+#-----------------------------------------
+# [DT] Estymacja kwantyli 0.05, 0.5, 0.95
+#-----------------------------------------
+# Wartość oczekiwana
+mu_dt <- mean(log_zwroty_dt)
+
+# Wariancja
+variance_dt <- var(log_zwroty_dt)
+
+# Odchylenie standardowe
+std_dev_dt <- sqrt(variance_dt)
+
+# Kwantyle
+quantiles_dt <- quantile(log_zwroty_dt, probs = c(0.05, 0.5, 0.95))
+
+mu_dt
+variance_dt
+std_dev_dt
+quantiles_dt
+
+
+# Tworzenie histogramu z naniesionymi liniami dla średniej i kwantyli
+wykres_kwantyli_dt <- ggplot(log_zwroty_dt_df, aes(x = log_zwroty_dt)) +
+  geom_histogram(binwidth = 0.01, fill = "grey", color = "black", alpha = 0.7) + # Histogram
+  geom_vline(aes(xintercept = mu_dt, color = "Średnia"), linetype = "dashed", size = 1) + # Średnia
+  geom_vline(aes(xintercept = quantiles_dt[1], color = "Kwantyl 5%"), linetype = "dotted", size = 1) + # Kwantyl 5%
+  geom_vline(aes(xintercept = quantiles_dt[2], color = "Mediana"), linetype = "dotted", size = 1) + # Mediana
+  geom_vline(aes(xintercept = quantiles_dt[3], color = "Kwantyl 95%"), linetype = "dotted", size = 1) + # Kwantyl 95%
+  scale_color_manual(name = "Legenda", 
+                     values = c("Średnia" = "red", 
+                                "Kwantyl 5%" = "blue", 
+                                "Mediana" = "green", 
+                                "Kwantyl 95%" = "orange")) + # Kolory i etykiety legendy
+  labs(title = NULL, x = "Log-Zwroty", y = "Gęstość") + # Tytuły osi i wykresu
+  theme_minimal() + # Minimalny styl wykresu
+  theme(legend.position = "top") # Ustawienie legendy na górze
+
+wykres_kwantyli_dt
+
+ggsave(
+  "img/estymacja_kwantyli_dt.png",
+  plot = wykres_kwantyli_dt,
+  width = 12,
+  height = 9,
+  units = "cm",
+  dpi = 480
+)
 
 
 #-----------------------------------------
-# Esytmacjia dystrybuanty empirycznej
+# [NWG] Esytmacjia dystrybuanty empirycznej
 #-----------------------------------------
 
 # Estymacja dystrybuanty empirycznej
@@ -286,10 +333,43 @@ ggsave(
 )
 
 #-----------------------------------------
-# Estymacja parametrów rozkładu normalnego i t-studenta
+# [DT] Esytmacjia dystrybuanty empirycznej
 #-----------------------------------------
 
-# nwg 
+# Estymacja dystrybuanty empirycznej
+dystrybuanta_empiryczna <- ecdf(log_zwroty_dt)
+
+# Wartości x dla wykresu
+x_wartosci <- seq(min(log_zwroty_dt), max(log_zwroty_dt), length.out = 100)
+
+# Wartości dystrybuanty empirycznej
+F_empiryczna <- dystrybuanta_empiryczna(x_wartosci)
+
+temp_df <- data.frame(x = x_wartosci, F = F_empiryczna)
+
+dystrybuanta_empiryczna_dt <- ggplot(temp_df, aes(x = x, y = F)) +
+  geom_line(color = "blue", size = 1) +
+  labs(
+    title = NULL,
+    x = "Log-Zwroty",
+    y = "Dystrybuanta Empiryczna F(x)"
+  ) +
+  theme_minimal()
+
+dystrybuanta_empiryczna_dt
+
+ggsave(
+  "img/dystrybuanta_empiryczna_dt.png",
+  plot = dystrybuanta_empiryczna_dt,
+  width = 12,
+  height = 9,
+  units = "cm",
+  dpi = 480
+)
+
+#-----------------------------------------
+# [NWG] Estymacja parametrów rozkładu normalnego i t-studenta
+#-----------------------------------------
 
 dist_norm_nwg <- fitdist(log_zwroty_nwg, "norm")
 dist_t_nwg <- fitdist(log_zwroty_nwg, "t", start = list(df = 12))
@@ -299,7 +379,9 @@ curve(dt(x, dist_t_nwg$estimate), xlim = c(-4, 4), col = 2, lwd = 2)
 dist_norm_nwg
 dist_t_nwg
 
-# dt
+#-----------------------------------------
+# [DT] Estymacja parametrów rozkładu normalnego i t-studenta
+#-----------------------------------------
 
 dist_norm_dt <- fitdist(log_zwroty_dt, "norm")
 dist_t_dt <- fitdist(log_zwroty_dt, "t", start = list(df = 12))
@@ -309,16 +391,15 @@ curve(dt(x, dist_t_dt$estimate), xlim = c(-4, 4), col = 2, lwd = 2)
 dist_norm_dt
 dist_t_dt
 
-
 #-----------------------------------------
-# Wykresy diagnostyczne
+# [NWG] Wykresy diagnostyczne
 #-----------------------------------------
 
 par(mfrow = c(1, 1))
-curve(dnorm(x, dist_norm$estimate[1], dist_norm$estimate[2]), xlim = c(-4, 4), lwd = 2)
-curve(dt(x, dist_t$estimate), add = T, col = 2, lwd = 2)
+curve(dnorm(x, dist_norm_nwg$estimate[1], dist_norm_nwg$estimate[2]), xlim = c(-4, 4), lwd = 2)
+curve(dt(x, dist_t_nwg$estimate), add = T, col = 2, lwd = 2)
 
-key <- c("norm", "t-student")
+key_nwg <- c("norm", "t-student")
 
 png(
   "img/wykresy_diagnostyczne_nwg.png",
@@ -329,57 +410,93 @@ png(
   res = 480
 )
 par(mfrow = c(2, 2))
-denscomp(list(dist_norm, dist_t), legendtext = key)
-qqcomp(list(dist_norm, dist_t), legendtext = key)
-cdfcomp(list(dist_norm, dist_t), legendtext = key)
-ppcomp(list(dist_norm, dist_t), legendtext = key)
+denscomp(list(dist_norm_nwg, dist_t_nwg), legendtext = key_nwg)
+qqcomp(list(dist_norm_nwg, dist_t_nwg), legendtext = key_nwg)
+cdfcomp(list(dist_norm_nwg, dist_t_nwg), legendtext = key_nwg)
+ppcomp(list(dist_norm_nwg, dist_t_nwg), legendtext = key_nwg)
 
 dev.off()
 
 
 #-----------------------------------------
-# Analiza wartości statystyk
+# [DT] Wykresy diagnostyczne
+#-----------------------------------------
+
+par(mfrow = c(1, 1))
+curve(dnorm(x, dist_norm_dt$estimate[1], dist_norm_dt$estimate[2]), xlim = c(-4, 4), lwd = 2)
+curve(dt(x, dist_t_dt$estimate), add = T, col = 2, lwd = 2)
+
+key_dt <- c("norm", "t-student")
+
+png(
+  "img/wykresy_diagnostyczne_dt.png",
+  width = 18,
+  height = 18,
+  pointsize = 9,
+  units = "cm",
+  res = 480
+)
+par(mfrow = c(2, 2))
+denscomp(list(dist_norm_dt, dist_t_dt), legendtext = key_dt)
+qqcomp(list(dist_norm_dt, dist_t_dt), legendtext = key_dt)
+cdfcomp(list(dist_norm_dt, dist_t_dt), legendtext = key_dt)
+ppcomp(list(dist_norm_dt, dist_t_dt), legendtext = key_dt)
+
+dev.off()
+
+
+#-----------------------------------------
+# [NWG] Analiza wartości statystyk
 #-----------------------------------------
 
 gofstat(
-  list(dist_norm, dist_t),
-  fitnames = key
+  list(dist_norm_nwg, dist_t_nwg),
+  fitnames = key_nwg
+)
+
+#-----------------------------------------
+# [DT] Analiza wartości statystyk
+#-----------------------------------------
+
+gofstat(
+  list(dist_norm_dt, dist_t_dt),
+  fitnames = key_dt
 )
 
 
 #-----------------------------------------
 # Zad 5
-# Test hipotezy o równości rozkładów
+# [NWG] Test hipotezy o równości rozkładów
 #-----------------------------------------
 
-iterations <- 10000
-n <- length(log_zwroty_nwg)
-n
+iterations_nwg <- 10000
+n_nwg <- length(log_zwroty_nwg)
+n_nwg
 
-D <- c()
+D_nwg <- c()
 
 for (i in 1:iterations) {
-  y_ln <- rnorm(n, dist_norm$estimate[1], dist_norm$estimate[2])
-  D[i] <- ks.test(
-    y_ln,
+  y_ln_nwg <- rnorm(n, dist_norm_nwg$estimate[1], dist_norm_nwg$estimate[2])
+  D_nwg[i] <- ks.test(
+    y_ln_nwg,
     pnorm,
-    dist_norm$estimate[1],
-    dist_norm$estimate[2],
+    dist_norm_nwg$estimate[1],
+    dist_norm_nwg$estimate[2],
     exact = TRUE
   )$statistic
 }
 
 # Obliczamy dn_ln, czyli wartosc statystyki D,
 # dla danych kurs_zamkniecia_nwg i rozkładu F0 wybranego w punkcie
-dn_n <- ks.test(
+dn_n_nwg <- ks.test(
   log_zwroty_nwg,
   pnorm,
-  dist_norm$estimate[1],
-  dist_norm$estimate[2],
+  dist_norm_nwg$estimate[1],
+  dist_norm_nwg$estimate[2],
   exact = TRUE
 )$statistic
 
-dn_n
+dn_n_nwg
 
 png(
   "img/hipoteza_o_rownosci_nwg.png",
@@ -390,8 +507,8 @@ png(
   res = 480
 )
 par(mfrow = c(1, 1))
-hist(D, prob = TRUE, xlab = "")
-points(dn_n, 0, pch = 19, col = "red")
+hist(D_nwg, prob = TRUE, xlab = "")
+points(dn_n_nwg, 0, pch = 19, col = "red")
 dev.off()
 
 
@@ -399,17 +516,76 @@ dev.off()
 # oraz dystrybuanty F0 jest istotnie większa od odleglosci obserwowanych
 # dla probek tej samej licznosci z rozkladu F0.
 
-p_value_n <- length(D[D > dn_n]) / iterations
-p_value_n
+p_value_n_nwg <- length(D_nwg[D_nwg > dn_n_nwg]) / iterations_nwg
+p_value_n_nwg
 
 
-alfa <- c(0.05)
-p_value_n <= alfa
+alfa_nwg <- c(0.05)
+p_value_n_nwg <= alfa_nwg
 # Wartosc p-value jest mniejsza od przyjetego poziomu istotnosci, zatem
 # hipoteze o rownosci dystrybuant (F = F0, gdzie F poszukiwany rozklad) odrzucam.
 
 
+#-----------------------------------------
+# Zad 5
+# [DT] Test hipotezy o równości rozkładów
+#-----------------------------------------
 
+iterations_dt <- 10000
+n_dt <- length(log_zwroty_dt)
+n_dt
+
+D_dt <- c()
+
+for (i in 1:iterations) {
+  y_ln_dt <- rnorm(n, dist_norm_dt$estimate[1], dist_norm_dt$estimate[2])
+  D_dt[i] <- ks.test(
+    y_ln_dt,
+    pnorm,
+    dist_norm_dt$estimate[1],
+    dist_norm_dt$estimate[2],
+    exact = TRUE
+  )$statistic
+}
+
+# Obliczamy dn_ln, czyli wartosc statystyki D,
+# dla danych kurs_zamkniecia_dt i rozkładu F0 wybranego w punkcie
+dn_n_dt <- ks.test(
+  log_zwroty_dt,
+  pnorm,
+  dist_norm_dt$estimate[1],
+  dist_norm_dt$estimate[2],
+  exact = TRUE
+)$statistic
+
+dn_n_dt
+
+png(
+  "img/hipoteza_o_rownosci_dt.png",
+  width = 9,
+  height = 12,
+  pointsize = 9,
+  units = "cm",
+  res = 480
+)
+par(mfrow = c(1, 1))
+hist(D_dt, prob = TRUE, xlab = "")
+points(dn_n_dt, 0, pch = 19, col = "red")
+dev.off()
+
+
+# Odleglosc dystrybuanty empirycznej dla kurs_zamkniecia_dt,
+# oraz dystrybuanty F0 jest istotnie większa od odleglosci obserwowanych
+# dla probek tej samej licznosci z rozkladu F0.
+
+p_value_n_dt <- length(D_dt[D_dt > dn_n_dt]) / iterations_dt
+p_value_n_dt
+
+
+alfa_dt <- c(0.05)
+p_value_n_dt <= alfa_dt
+# Wartosc p-value jest mniejsza od przyjetego poziomu istotnosci, zatem
+# hipoteze o rownosci dystrybuant (F = F0, gdzie F poszukiwany rozklad) odrzucam.
 
 
 #-----------------------------------------
@@ -439,7 +615,7 @@ m_log_kursu_dyn <- log(merged_kursy$kurs.y)
 m_log_zwroty_nwg <- diff(m_log_kursu_nwg)
 m_log_zwroty_dyn <- diff(m_log_kursu_dyn)
 
-?diff
+# ?diff
 
 # Tworzenie wykresów na podstawie danych
 library(ggExtra)
